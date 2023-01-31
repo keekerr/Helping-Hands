9466c5e90b1a1892119d14247e7b40d83d2e37f5
keekerr <keeley.s.sprouse@gmail.com>
2023-01-30 19:08:54 -0500

    Initial commit for Model

--- models/index.js
+++ models/index.js
@@ -1,1 +1,6 @@
 // empty model to be filled in 
+const User = require('./User');
+const Volunteer = require('.Volunteer');
+const Event = require('./Event');
+
+// Add in the relationships between the table (i.e. one user can have many event but each event only has one user (admin))
