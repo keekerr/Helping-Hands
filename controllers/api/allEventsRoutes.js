8a22a894ebac9a773e342de684472878d7155246
difurung <dfurung@hotmail.com>
2023-02-02 16:50:29 -0500

    Added Get single event to allEventsRoutes

--- controllers/api/allEventsRoutes.js
+++ controllers/api/allEventsRoutes.js
@@ -37,4 +37,34 @@ router.get ('/all-event-details', async (req, res) => {
 
 // Should we do a GET for single events?? then show modal??? 
 
+
+// router.get('/modal???', async (req, res) => {
+//   try {
+//     // Get all  My events          //or findAll?? then filter with where?? 
+//     const eventData = await Event.findByPK(req.params.user_id, {
+//       model: Event ,
+//       attributes: ['id', 'name', 'category', 'volunteersNeed', 'volunteersSignedUp' ],
+//       order: [['category', 'ASC']],
+//       // where clause for user_id
+//       where: {
+//         user_id: req.params.user_id
+//       }
+      
+//     });
+
+//      // Serialize user data so templates can read it
+//     const events = eventData.map((event) =>{ 
+//         event.get({ plain: true })
+//         //TODO: Calculate the percentage and store it in progressPercentage
+//         const progressPercentage = event.volunteersNeed / event.volunteersSignedUp * 100
+
+//         return {...event, progressPercentage }
+//     });
+
+//     // Pass serialized data into Handlebars.js template
+//     res.render('my-event', { events });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 // Delete if there is time 
