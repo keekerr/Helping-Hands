755cff83455bf7b11014d4545f8c7ba6b8ab8667
unknown <lexiarbajianclt@gmail.com>
2023-02-04 13:37:55 -0500

    styling for event cards and dash done - taking a lot from those for homepage, specific event details and main.hbs

--- public/js/miscBtn.js
+++ public/js/miscBtn.js
@@ -1,6 +1,10 @@
 //see all event buttons (goes to homepage)
 var allEventsBtn= document.getElementById("allEventsBtn");
 
+allEventsBtn.addEventListener("click", function(){
+    
+})
+
 
 //my events (goes to dashboard)
 
