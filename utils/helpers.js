21196cf9c4b921e9ffbbc319b6400dfbe77fd4d8
difurung <dfurung@hotmail.com>
2023-02-05 16:06:18 -0500

    Got homeroutes working. Commented out format time helper.

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,8 @@
-module.exports = {
-    format_time: (date) => {
-      return date.toLocaleTimeString();
+// module.exports = {
+//     format_time: (date) => {
+//       return date.toLocaleTimeString();
     
-    },
+//     },
   
 
-}
+// }
