9e87b0c0c748a12091469ff8b737c762d7d0fb77
beattycharles <113803621+beattycharles@users.noreply.github.com>
2023-02-05 19:49:17 -0500

    undid changes

--- public/js/login.js
+++ public/js/login.js
@@ -10,7 +10,7 @@ const loginFormHandler = async (event) => {
 
   if (email && password) {
     // Send the e-mail and password to the server
-    const response = await fetch("/api/user", {
+    const response = await fetch("/api/users", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
