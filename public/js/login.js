2762fd808afab8a55a728bcc46c532d060e2c10a
difurung <dfurung@hotmail.com>
2023-02-05 22:30:37 -0500

    added event listener to login.js

--- public/js/login.js
+++ public/js/login.js
@@ -26,6 +26,38 @@ const loginFormHandler = async (event) => {
   }
 };
 
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const fName = document.querySelector("#fname").value.trim();
+  const lName = document.querySelector("#fname").value.trim();
+  const birthday = document.querySelector("#birthday").value.trim();
+  const state = document.querySelector("#state").value.trim();
+  const userZip = document.querySelector("#userzip").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#passwor").value.trim();
+
+  if (fName && lName && birthday && state && userZip && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
 document
   .querySelector("#login-form")
   .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector("#signup-form")
+  .addEventListener("submit", signupFormHandler);
+
+  
