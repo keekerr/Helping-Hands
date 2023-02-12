94f69c46752fabd9d8157c51975fa1289b9220f5
beattycharles <113803621+beattycharles@users.noreply.github.com>
2023-02-11 22:17:56 -0500

    fix spelling error

--- public/js/acctEvent.js
+++ public/js/acctEvent.js
@@ -1,54 +1,47 @@
-
-
 //saves new event with submission button
 // var eventSubmission = document.getElementById("event_submission");
 
 const createEvent = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const event_name = document.querySelector('#event_name').value.trim();
-    const event_date = document.querySelector('#event_date').value.trim();
-    const event_type = document.querySelector('#event_type').value.trim();
-    const vol_need = document.querySelector('#volnum').value.trim();
-    const event_address = document.querySelector('#address').value.trim();
-    const event_description = document.querySelector('#event_description').value.trim();
+  const event_name = document.querySelector("#event_name").value.trim();
+  const event_date = document.querySelector("#event_date").value.trim();
+  const event_type = document.querySelector("#event_type").value.trim();
+  const vol_need = document.querySelector("#volnum").value.trim();
+  const event_address = document.querySelector("#address").value.trim();
+  const event_description = document
+    .querySelector("#event_description")
+    .value.trim();
 
-    if (
-      event_name &&
-      event_date &&
-      event_type &&
-      vol_need &&
-      event_address &&
-      event_description
-    ) {
-      const createNewEvent = await fetch("/api/myEvents", {
-        method: "POST",
-        body: JSON.stringify({
-          event_name,
-          event_date,
-          event_type,
-          vol_need,
-          event_address,
-          event_description,
-        }),
-        headers: { "New-Event": "application/json" },
-      });
-console.log("are you working")
-      if (response.ok) {
-        document.location("/dashboard");
-      } else {
-        alert(createNewEvent.statusText);
-      }
+  if (
+    event_name &&
+    event_date &&
+    event_type &&
+    vol_need &&
+    event_address &&
+    event_description
+  ) {
+    const createNewEvent = await fetch("/api/myEvents", {
+      method: "POST",
+      body: JSON.stringify({
+        event_name,
+        event_date,
+        event_type,
+        vol_need,
+        event_address,
+        event_description,
+      }),
+      headers: { "New-Event": "application/json" },
+    });
+    console.log("are you working");
+    if (Response.ok) {
+      document.location("/dashboard");
+    } else {
+      alert(createNewEvent.statusText);
     }
+  }
 };
-
-
 
 document
   .querySelector("#event_submission")
   .addEventListener("click", createEvent);
-
-
-
-
-
